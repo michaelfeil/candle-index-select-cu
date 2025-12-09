817b209f69b5fa75602e53e99177443fc1a87d27
michaelfeil <me@michaelfeil.eu>
2025-12-09 07:27:59 +0000

    add faster candiate, leave canidate 1 behind

--- benches/bench_index_select.rs
+++ benches/bench_index_select.rs
@@ -155,6 +155,7 @@ fn run_benchmark<S: Into<Shape> + Clone>(
 
 fn bench_index_select(c: &mut Criterion) {
     run_benchmark(c, "index_select_short_2d", (100, 128), 200);
+    run_benchmark(c, "index_select_mid_2d", (16_000, 1024), 12_000);
     run_benchmark(c, "index_select_long_2d", (16_000, 1024), 70_000);
     run_benchmark(c, "index_select_very_long_2d", (100_000, 2048), 500_000);
     run_benchmark(c, "index_select_3d", (10, 100, 128), 200);
